d07b6a3b915f13d30246d56ef06fd986edc70560
Vijay Cheleenahalli <cnvijayrao@gmail.com>
2021-03-21 21:43:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ let timer = 0
 input.onButtonPressed(Button.A, function () {
     timer = input.runningTime()
 })
+/**
+ * stop watch using Microbit. Converting milliseconds into seconds by dividing the elapsed by 1000
+ */
 input.onButtonPressed(Button.B, function () {
     basic.showNumber((input.runningTime() - timer) / 1000)
 })
